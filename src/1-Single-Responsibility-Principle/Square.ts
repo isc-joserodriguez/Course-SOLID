81f0ce1cf7e9c235b432fe1dbb49bc51f9965f0b
Jose Rodriguez <j.rodriguez@blackstone.studio>
2022-09-21 12:01:41 -0600

    Low cohesion level on Square

---
+++ src/1-Single-Responsibility-Principle/Square.ts
@@ -0,0 +1,25 @@
+export default class Square {
+  side: number = 5;
+  highResolutionMonitor: boolean = true;
+
+  calculateArea(): number {
+    return this.side * this.side;
+  }
+
+  calculatePerimeter(): number {
+    return this.side * 4;
+  }
+
+  draw(): void {
+    if(this.highResolutionMonitor){
+        // Render a high resolution image of a square
+    } else {
+        // Render a normal image of a square
+    }
+  }
+
+  rotate(degree: number): void {
+    // Rotate the image of the square clockwise to
+    // the required degree and re-render
+  }
+}
